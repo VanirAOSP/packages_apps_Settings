fdb1f5606fb89374226dac835e9b1be15d36f90d
nuclearmistake <nuclearmistake@gmail.com>
2013-02-17 21:45:48 -0500

    Touch DEEZ 7 times to become a developer.
    Change-Id: I6c59f44e4506b854f1dd27a23b9d11351193eae5

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -416,9 +416,7 @@ public class Settings extends PreferenceActivity
     }
 
     private void updateHeaderList(List<Header> target) {
-        final boolean showDev = mDevelopmentPreferences.getBoolean(
-                DevelopmentSettings.PREF_SHOW,
-                android.os.Build.TYPE.equals("eng") || android.os.Build.TYPE.equals("userdebug"));
+        final boolean showDev = true;
         int i = 0;
 
         mHeaderIndexMap.clear();
