f75f4fcebd771dd867bf5c5064dfaaf454180d20
Darren Salt <devspam@moreofthesa.me.uk>
2013-06-27 01:15:12 +0100

    Handle cancellation of the 'enabling PrivacyGuard' alert box.
    I noticed that the option could remain ticked but not actually enabled
    if the alert box is cancelled.
    
    Change-Id: If9a4e88641a5e4283d6ea16c17ca4461841580e3

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -1202,8 +1202,7 @@ public class InstalledAppDetails extends Fragment
                     .setNegativeButton(R.string.dlg_cancel,
                         new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
-                            // Re-enable the checkbox
-                            getOwner().mNotificationSwitch.setChecked(true);
+                            dialog.cancel();
                         }
                     })
                     .create();
@@ -1236,13 +1235,26 @@ public class InstalledAppDetails extends Fragment
                     .setNegativeButton(R.string.dlg_cancel,
                         new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
-                            // Re-enable the checkbox
-                            getOwner().mPrivacyGuardSwitch.setChecked(false);
+                            dialog.cancel();
                         }
                     })
                     .create();
             }
             throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_DISABLE_NOTIFICATIONS:
+                    // Re-enable the checkbox
+                    getOwner().mNotificationSwitch.setChecked(true);
+                case DLG_PRIVACY_GUARD:
+                    // Re-enable the checkbox
+                    getOwner().mPrivacyGuardSwitch.setChecked(false);
+                    break;
+            }
         }
     }
 
