ef1a0c5ede9576f9eb4d64f32e4f5425fb5d009b
DHOMD <DHO@dho.im>
2013-09-12 16:38:50 -0400

    Give me one more }
    Needed to fix build and fix and complete the NPE fix
    
    Change-Id: Ie0c5ef1dcf70d7f348478eea52628cbc289b6ddd

--- src/com/android/settings/vanir/LockscreenTargets.java
+++ src/com/android/settings/vanir/LockscreenTargets.java
@@ -393,6 +393,7 @@ public class LockscreenTargets extends Fragment implements ShortcutPickHelper.On
     public void shortcutPicked(String uri, String friendlyName, boolean isApplication) {
         if (uri == null) {
             return;
+            }
         try {
             Intent i = Intent.parseUri(uri, 0);
             PackageManager pm = mActivity.getPackageManager();
