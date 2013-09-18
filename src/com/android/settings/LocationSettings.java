6386f9b37bea0a580a9f76aae032778cda2e60ae
Michael Zhou <mzhou@cse.unsw.edu.au>
2013-09-18 12:50:30 +1000

    Add external bluetooth GPS support (2/2)
    Originally by
    Cuong Bui <cuongbui@gmail.com>
    Benjamin Bunse <ich@morphew.de>
    
    http://review.cyanogenmod.org/#/c/20849/
    http://review.cyanogenmod.org/#/c/20850/
    
    http://review.cyanogenmod.org/#/c/36809/
    http://review.cyanogenmod.org/#/c/36810/
    http://review.cyanogenmod.org/#/c/36811/
    
    Change-Id: Ia88ad9c70f89a09d308d2cccf9860c4d35470b24

--- src/com/android/settings/LocationSettings.java
+++ src/com/android/settings/LocationSettings.java
@@ -17,6 +17,8 @@
 package com.android.settings;
 
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.ContentQueryMap;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -25,6 +27,7 @@ import android.database.Cursor;
 import android.location.LocationManager;
 import android.os.UserManager;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
@@ -32,7 +35,9 @@ import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -52,6 +57,7 @@ public class LocationSettings extends SettingsPreferenceFragment
     private CheckBoxPreference mGps;
     private CheckBoxPreference mAssistedGps;
     private SwitchPreference mLocationAccess;
+    private ListPreference mGPSBTPref;
 
     // These provide support for receiving notification when Location Manager settings change.
     // This is necessary because the Network Location Provider can change settings
@@ -92,6 +98,30 @@ public class LocationSettings extends SettingsPreferenceFragment
         mNetwork = (CheckBoxPreference) root.findPreference(KEY_LOCATION_NETWORK);
         mGps = (CheckBoxPreference) root.findPreference(KEY_LOCATION_GPS);
         mAssistedGps = (CheckBoxPreference) root.findPreference(KEY_ASSISTED_GPS);
+
+        // add BT gps devices
+        mGPSBTPref = (ListPreference) findPreference("location_gps_source");
+        ArrayList<CharSequence> entries = new ArrayList<CharSequence>();
+        for (String e : getResources().getStringArray(R.array.location_entries_gps_source)) {
+            entries.add(e);
+        }
+        ArrayList<CharSequence> values = new ArrayList<CharSequence>();
+        for (String v : getResources().getStringArray(R.array.location_values_gps_source)) {
+            values.add(v);
+        }
+        // add known bonded BT devices
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if ((mBluetoothAdapter != null) && (mBluetoothAdapter.isEnabled())) {
+            for (BluetoothDevice d : mBluetoothAdapter.getBondedDevices()) {
+                String dname = d.getName() + " - " + d.getAddress();
+                entries.add(dname);
+                values.add(d.getAddress());
+            }
+        }
+        mGPSBTPref.setEntries(entries.toArray(new CharSequence[entries.size()]));
+        mGPSBTPref.setEntryValues(values.toArray(new CharSequence[values.size()]));
+        mGPSBTPref.setDefaultValue("0");
+        mGPSBTPref.setOnPreferenceChangeListener(this);
 
         // Only enable these controls if this user is allowed to change location
         // sharing settings.
@@ -202,6 +232,22 @@ public class LocationSettings extends SettingsPreferenceFragment
     public boolean onPreferenceChange(Preference pref, Object newValue) {
         if (pref.getKey().equals(KEY_LOCATION_TOGGLE)) {
             onToggleLocationAccess((Boolean) newValue);
+        } else if (pref == mGPSBTPref) {
+            String oldPref = Settings.System.getString(getContentResolver(),
+                    Settings.Secure.EXTERNAL_GPS_BT_DEVICE);
+            String newPref = newValue == null ? "0" : (String) newValue;
+            // "0" represents the internal GPS.
+            Settings.System.putString(getContentResolver(), Settings.Secure.EXTERNAL_GPS_BT_DEVICE,
+                    newPref);
+            if (!oldPref.equals(newPref) && ("0".equals(oldPref) || "0".equals(newPref))) {
+                LocationManager locationManager =
+                        (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+                locationManager.setGPSSource(newPref);
+                // Show msg to inform user that source has been switched
+                Toast.makeText(this.getActivity(),
+                        getResources().getString(R.string.location_gps_source_notification),
+                        Toast.LENGTH_LONG).show();
+            }
         }
         return true;
     }
