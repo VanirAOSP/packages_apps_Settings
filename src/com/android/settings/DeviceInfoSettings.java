20498268ba7acd3f2844a49dd14f685a2c1e9a0c
nuclearmistake <nuclearmistake@gmail.com>
2013-11-19 23:30:11 -0500

    Fix modversion in device info. Kill CMUpdater, CMStats, and Changelog
    Change-Id: I9e8154d5a3787b9b0d2f8ab6c7a6c84ba9aa8bd6

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -100,7 +100,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
-        setValueSummary(KEY_MOD_VERSION, "ro.build.version");
+        setValueSummary(KEY_MOD_VERSION, "ro.modversion");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
 
