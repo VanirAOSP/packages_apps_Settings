649df732f2e781d569deaf66660951d5c4e590f2
PrimeDirective <activethrasher00@gmail.com>
2013-12-13 21:18:48 -0500

    Revert "keen observation: they're the same thing."
    This reverts commit 7447711f405a846019cc656f188b703a34f5acb1.
    
    Change-Id: I1e440868e43299438c70320dd4d4a35bd5df8bf9

--- src/com/android/settings/cyanogenmod/SystemUiSettings.java
+++ src/com/android/settings/cyanogenmod/SystemUiSettings.java
@@ -46,12 +46,18 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
     private static final String KEY_EXPANDED_DESKTOP = "expanded_desktop";
     private static final String KEY_EXPANDED_DESKTOP_NO_NAVBAR = "expanded_desktop_no_navbar";
     private static final String CATEGORY_NAVBAR = "navigation_bar";
+    private static final String KEY_PIE_CONTROL = "pie_control";
     private static final String KEY_SCREEN_GESTURE_SETTINGS = "touch_screen_gesture_settings";
+    private static final String KEY_IMMERSIVE_MODE_STYLE = "immersive_mode_style";
+    private static final String KEY_IMMERSIVE_MODE_STATE = "immersive_mode_state";
     private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
     private static final String KEY_WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
 
+    private PreferenceScreen mPieControl;
     private ListPreference mExpandedDesktopPref;
     private CheckBoxPreference mExpandedDesktopNoNavbarPref;
+    private ListPreference mImmersiveModePref;
+    private CheckBoxPreference mImmersiveModeState;
     private Preference mCustomLabel;
     private CheckBoxPreference mWakeWhenPluggedOrUnplugged;
 
@@ -62,19 +68,19 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.system_ui_settings);
-        PreferenceScreen prefScreen = getPreferenceScreen();
+        PreferenceScreen prefSet = getPreferenceScreen();
 
         mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
-        // Expanded desktop
-        mExpandedDesktopPref = (ListPreference) findPreference(KEY_EXPANDED_DESKTOP);
-        mExpandedDesktopNoNavbarPref =
-                (CheckBoxPreference) findPreference(KEY_EXPANDED_DESKTOP_NO_NAVBAR);
 
-        Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
-                getPreferenceScreen(), KEY_SCREEN_GESTURE_SETTINGS);
-
-        int expandedDesktopValue = Settings.System.getInt(getContentResolver(),
-                Settings.System.EXPANDED_DESKTOP_STYLE, 0);
+        mImmersiveModeState = (CheckBoxPreference) findPreference(KEY_IMMERSIVE_MODE_STATE);
+        mImmersiveModeState.setChecked(Settings.System.getInt(getContentResolver(), 
+                    Settings.System.GLOBAL_IMMERSIVE_MODE_STATE, 0) == 1);
+        mImmersiveModeState.setOnPreferenceChangeListener(this);        
+        
+        mImmersiveModePref = (ListPreference) prefSet.findPreference(KEY_IMMERSIVE_MODE_STYLE);
+        mImmersiveModePref.setOnPreferenceChangeListener(this);
+        int immersiveModeValue = Settings.System.getInt(getContentResolver(), Settings.System.GLOBAL_IMMERSIVE_MODE_STYLE, 0);
+        mImmersiveModePref.setValue(String.valueOf(immersiveModeValue));
 
         // Default value for wake-on-plug behavior from config.xml
         boolean wakeUpWhenPluggedOrUnpluggedConfig = getResources().getBoolean(
@@ -88,60 +94,125 @@ public class SystemUiSettings extends SettingsPreferenceFragment  implements
         try {
             boolean hasNavBar = WindowManagerGlobal.getWindowManagerService().hasNavigationBar();
 
-            if (hasNavBar) {
-                mExpandedDesktopPref.setOnPreferenceChangeListener(this);
-                mExpandedDesktopPref.setValue(String.valueOf(expandedDesktopValue));
-                updateExpandedDesktop(expandedDesktopValue);
-                prefScreen.removePreference(mExpandedDesktopNoNavbarPref);
-            } else {
-                // Hide no-op "Status bar visible" expanded desktop mode
-                mExpandedDesktopNoNavbarPref.setOnPreferenceChangeListener(this);
-                mExpandedDesktopNoNavbarPref.setChecked(expandedDesktopValue > 0);
-                prefScreen.removePreference(mExpandedDesktopPref);
-                // Hide navigation bar category
-                prefScreen.removePreference(findPreference(CATEGORY_NAVBAR));
+//            lets keep this around so i can modify it to act accordingly with hardware key only devices
+//             Hide no-op "Status bar visible" mode on devices without navigation bar
+//            if (hasNavBar) {
+//                mExpandedDesktopPref.setOnPreferenceChangeListener(this);
+//                mExpandedDesktopPref.setValue(String.valueOf(expandedDesktopValue));
+//                updateExpandedDesktop(expandedDesktopValue);
+//                prefScreen.removePreference(mExpandedDesktopNoNavbarPref);
+//            } else {
+//                mExpandedDesktopNoNavbarPref.setOnPreferenceChangeListener(this);
+//                mExpandedDesktopNoNavbarPref.setChecked(expandedDesktopValue > 0);
+//                prefScreen.removePreference(mExpandedDesktopPref);
+//            }
+//
+
+            // Hide navigation bar category on devices without navigation bar
+            if (!hasNavBar) {
+                prefSet.removePreference(findPreference(CATEGORY_NAVBAR));
             }
         } catch (RemoteException e) {
             Log.e(TAG, "Error getting navigation bar status");
         }
+
+        updateCustomLabelTextSummary();
+        updateImmersiveModeState(immersiveModeValue);
+        updateImmersiveModeSummary(immersiveModeValue);
+    }
+
+    private void updateImmersiveModeState(int value) {
+        if (value >=1) {
+            mImmersiveModeState.setEnabled(true);
+        } else {
+            mImmersiveModeState.setEnabled(false);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mExpandedDesktopPref) {
-            int expandedDesktopValue = Integer.valueOf((String) objValue);
-            updateExpandedDesktop(expandedDesktopValue);
-            return true;
-        } else if (preference == mExpandedDesktopNoNavbarPref) {
-            boolean value = (Boolean) objValue;
-            updateExpandedDesktop(value ? 2 : 0);
+        if (preference == mImmersiveModePref) {
+            int immersiveModeValue = Integer.valueOf((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.GLOBAL_IMMERSIVE_MODE_STYLE, immersiveModeValue);
+             updateImmersiveModeSummary(immersiveModeValue);
+             updateImmersiveModeState(immersiveModeValue);
+             return true;
+        } else if (preference == mImmersiveModeState) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.GLOBAL_IMMERSIVE_MODE_STATE,
+                    (Boolean) objValue ? 1 : 0);
             return true;
         }
-
         return false;
     }
 
-    private void updateExpandedDesktop(int value) {
-        ContentResolver cr = getContentResolver();
-        Resources res = getResources();
-        int summary = -1;
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
 
-        Settings.System.putInt(cr, Settings.System.EXPANDED_DESKTOP_STYLE, value);
+        if (preference == mCustomLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
 
-        if (value == 0) {
-            // Expanded desktop deactivated
-            Settings.System.putInt(cr, Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED, 0);
-            Settings.System.putInt(cr, Settings.System.EXPANDED_DESKTOP_STATE, 0);
-            summary = R.string.expanded_desktop_disabled;
-        } else if (value == 1) {
-            Settings.System.putInt(cr, Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED, 1);
-            summary = R.string.expanded_desktop_status_bar;
-        } else if (value == 2) {
-            Settings.System.putInt(cr, Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED, 1);
-            summary = R.string.expanded_desktop_no_status_bar;
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
+            alert.setView(input);
+            alert.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+ 
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = ((Spannable) input.getText()).toString();
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.CUSTOM_CARRIER_LABEL, value);
+                    updateCustomLabelTextSummary();
+                    Intent i = new Intent();
+                    i.setAction("com.android.settings.LABEL_CHANGED");
+                    mContext.sendBroadcast(i);
+                }
+            });
+            alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
+        } else if (preference == mWakeWhenPluggedOrUnplugged) {
+            Settings.Global.putInt(getContentResolver(),
+                    Settings.Global.WAKE_WHEN_PLUGGED_OR_UNPLUGGED,
+                    mWakeWhenPluggedOrUnplugged.isChecked() ? 1 : 0);
+            return true;
         }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
 
-        if (mExpandedDesktopPref != null && summary != -1) {
-            mExpandedDesktopPref.setSummary(res.getString(summary));
+    private void updateImmersiveModeSummary(int value) {
+        Resources res = getResources();
+        if (value == 0) {
+            /* expanded desktop deactivated */
+            mImmersiveModePref.setSummary(res.getString(R.string.immersive_mode_disabled));
+        } else if (value == 1) {
+            String statusBarPresent = res.getString(R.string.immersive_mode_summary_status_bar);
+            mImmersiveModePref.setSummary(res.getString(R.string.summary_immersive_mode, statusBarPresent));
+        } else if (value == 2) {
+            String statusBarPresent = res.getString(R.string.immersive_mode_summary_no_status_bar);
+            mImmersiveModePref.setSummary(res.getString(R.string.summary_immersive_mode, statusBarPresent));
         }
     }
+
+    private void updateCustomLabelTextSummary() {
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL);
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
+        }
+     }   
 }
