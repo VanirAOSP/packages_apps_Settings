888e10300e36e810c7f6991b960050f064504a7c
nuclearmistake <nuclearmistake@gmail.com>
2014-02-10 21:29:57 -0500

    Revert "add option to force high-end graphics on low memory devices"
    This reverts commit ae175cfe7f198d544b424a9e43cf21c97be93e44.

--- src/com/android/settings/cyanogenmod/PerformanceSettings.java
+++ src/com/android/settings/cyanogenmod/PerformanceSettings.java
@@ -16,7 +16,6 @@
 
 package com.android.settings.cyanogenmod;
 
-import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
@@ -47,12 +46,8 @@ public class PerformanceSettings extends SettingsPreferenceFragment implements
 
     private static final String USE_16BPP_ALPHA_PROP = "persist.sys.use_16bpp_alpha";
 
-    private static final String FORCE_HIGHEND_GFX_PREF = "pref_force_highend_gfx";
-    private static final String FORCE_HIGHEND_GFX_PERSIST_PROP = "persist.sys.force_highendgfx";
-
     private ListPreference mPerfProfilePref;
     private CheckBoxPreference mUse16bppAlphaPref;
-    private CheckBoxPreference mForceHighEndGfx;
 
     private String[] mPerfProfileEntries;
     private String[] mPerfProfileValues;
@@ -106,14 +101,6 @@ public class PerformanceSettings extends SettingsPreferenceFragment implements
         String use16bppAlpha = SystemProperties.get(USE_16BPP_ALPHA_PROP, "0");
         mUse16bppAlphaPref.setChecked("1".equals(use16bppAlpha));
 
-        if (ActivityManager.isLowRamDeviceStatic()) {
-            mForceHighEndGfx = (CheckBoxPreference) prefSet.findPreference(FORCE_HIGHEND_GFX_PREF);
-            String forceHighendGfx = SystemProperties.get(FORCE_HIGHEND_GFX_PERSIST_PROP, "false");
-            mForceHighEndGfx.setChecked("true".equals(forceHighendGfx));
-        } else {
-            prefSet.removePreference(findPreference(FORCE_HIGHEND_GFX_PREF));
-        }
-
         /* Display the warning dialog */
         alertDialog = new AlertDialog.Builder(getActivity()).create();
         alertDialog.setTitle(R.string.performance_settings_warning_title);
@@ -154,9 +141,6 @@ public class PerformanceSettings extends SettingsPreferenceFragment implements
         if (preference == mUse16bppAlphaPref) {
             SystemProperties.set(USE_16BPP_ALPHA_PROP,
                     mUse16bppAlphaPref.isChecked() ? "1" : "0");
-        } else if (preference == mForceHighEndGfx) {
-            SystemProperties.set(FORCE_HIGHEND_GFX_PERSIST_PROP,
-                    mForceHighEndGfx.isChecked() ? "true" : "false");
         } else {
             // If we didn't handle it, let preferences handle it.
             return super.onPreferenceTreeClick(preferenceScreen, preference);
