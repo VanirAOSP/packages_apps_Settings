3f9467a5fd7382dbe30e636fb908b2a4f1cb1b7c
nuclearmistake <nuclearmistake@gmail.com>
2014-03-07 10:19:43 -0500

    Make navbar height+width bounds relative to their dimens in com.android.internal
    ps2: fix a copy/pasta derp
         fix some whitespace
    ps3: fix more derps
         fix more whitespace
    
    Signed-off-by: nuclearmistake <nuclearmistake@gmail.com>
    
    Change-Id: I17a0a542c34fa6d9da1b55492b8d61f955d685e6

--- src/com/android/settings/vanir/navbar/NavbarSettingsFragment.java
+++ src/com/android/settings/vanir/navbar/NavbarSettingsFragment.java
@@ -3,6 +3,7 @@ package com.android.settings.vanir.navbar;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -39,15 +40,20 @@ public class NavbarSettingsFragment extends Fragment implements SeekBar.OnSeekBa
     private TextView mBarWidth_Text;
 
     private Switch mEnabledSwitch;
-    private static int deviceKeys = 0;
+    private int deviceKeys = 0;
 
     private static int HValue;
     private static int LValue;
     private static int WValue;
-    private static int defNavBarSize;
-    private static int preSizer;
-    private static int minimumBarSize;
     private static boolean firstShot = true;
+    private static int mDefaultHeight;
+    private static int mDefaultHeightLandscape;
+    private static int mDefaultWidth;
+
+    private static final double MIN_HEIGHT_SCALAR = 0.25;
+    private static final double MAX_HEIGHT_SCALAR = 3.0;
+    private static final double MIN_WIDTH_SCALAR = 0.5;
+    private static final double MAX_WIDTH_SCALAR = 1.0;
 
     public NavbarSettingsFragment() {
     }
@@ -56,15 +62,12 @@ public class NavbarSettingsFragment extends Fragment implements SeekBar.OnSeekBa
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        deviceKeys = getResources().getInteger(
-                com.android.internal.R.integer.config_deviceHardwareKeys);
+        deviceKeys = getResources().getInteger(com.android.internal.R.integer.config_deviceHardwareKeys);
 
         if (deviceKeys > 0) {
-            final Activity activity = getActivity();
-            mEnabledSwitch = new Switch(activity);
+            mEnabledSwitch = new Switch(getActivity());
 
-            final int padding = activity.getResources().getDimensionPixelSize(
-                    R.dimen.action_bar_switch_padding);
+            final int padding = getActivity().getResources().getDimensionPixelSize(R.dimen.action_bar_switch_padding);
             mEnabledSwitch.setPaddingRelative(0, 0, padding, 0);
             mEnabledSwitch.setOnCheckedChangeListener(this);
         }
@@ -74,10 +77,9 @@ public class NavbarSettingsFragment extends Fragment implements SeekBar.OnSeekBa
     public void onStart() {
         super.onStart();
         if (deviceKeys > 0) {
-            final Activity activity = getActivity();
-            activity.getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM,
+            getActivity().getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM,
                     ActionBar.DISPLAY_SHOW_CUSTOM);
-            activity.getActionBar().setCustomView(mEnabledSwitch, new ActionBar.LayoutParams(
+            getActivity().getActionBar().setCustomView(mEnabledSwitch, new ActionBar.LayoutParams(
                     ActionBar.LayoutParams.WRAP_CONTENT,
                     ActionBar.LayoutParams.WRAP_CONTENT,
                     Gravity.CENTER_VERTICAL | Gravity.END));
@@ -90,9 +92,8 @@ public class NavbarSettingsFragment extends Fragment implements SeekBar.OnSeekBa
     public void onStop() {
         super.onStop();
         if (deviceKeys > 0) {
-            final Activity activity = getActivity();
-            activity.getActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_CUSTOM);
-            activity.getActionBar().setCustomView(null);
+            getActivity().getActionBar().setDisplayOptions(0, ActionBar.DISPLAY_SHOW_CUSTOM);
+            getActivity().getActionBar().setCustomView(null);
         }
     }
 
@@ -135,7 +136,35 @@ public class NavbarSettingsFragment extends Fragment implements SeekBar.OnSeekBa
         mBarWidth_Text = (TextView) v.findViewById(R.id.navigation_bar_width_text);
         mBarWidth_Text.setText(getString(R.string.navigation_bar_width_text));
 
-        initHeightValues();
+        if (firstShot) {
+            final Resources res = getActivity().getResources();
+            mDefaultHeight = res.getDimensionPixelSize(com.android.internal.R.dimen.navigation_bar_height);
+            mDefaultHeightLandscape = res.getDimensionPixelSize(com.android.internal.R.dimen.navigation_bar_height_landscape);
+            mDefaultWidth = res.getDimensionPixelSize(com.android.internal.R.dimen.navigation_bar_width);
+            firstShot = false;
+        }
+
+        final ContentResolver cr = getActivity().getContentResolver();
+        HValue = Settings.System.getInt(cr, Settings.System.NAVIGATION_BAR_HEIGHT, mDefaultHeight);
+        LValue = Settings.System.getInt(cr, Settings.System.NAVIGATION_BAR_HEIGHT_LANDSCAPE, mDefaultHeightLandscape);
+        WValue = Settings.System.getInt(cr, Settings.System.NAVIGATION_BAR_WIDTH, mDefaultWidth);
+
+        final int currentHeightPercent = (int)(100.0 *
+                ( HValue - MIN_HEIGHT_SCALAR * mDefaultHeight) /
+                ( MAX_HEIGHT_SCALAR * mDefaultHeight - MIN_HEIGHT_SCALAR * mDefaultHeight ));
+        final int currentHeightLandscapePercent = (int)(100.0 *
+                ( LValue - MIN_HEIGHT_SCALAR * mDefaultHeightLandscape) /
+                ( MAX_HEIGHT_SCALAR * mDefaultHeightLandscape - MIN_HEIGHT_SCALAR * mDefaultHeightLandscape ));
+        final int currentWidthPercent = (int)(100.0 *
+                ( WValue - MIN_WIDTH_SCALAR * mDefaultWidth) /
+                ( MAX_WIDTH_SCALAR * mDefaultWidth - MIN_WIDTH_SCALAR * mDefaultWidth ));
+
+        mNavigationBarHeight.setProgress(currentHeightPercent);
+        mNavigationBarHeight.setOnSeekBarChangeListener(this);
+        mNavigationBarHeightLandscape.setProgress(currentHeightLandscapePercent);
+        mNavigationBarHeightLandscape.setOnSeekBarChangeListener(this);
+        mNavigationBarWidth.setProgress(currentWidthPercent);
+        mNavigationBarWidth.setOnSeekBarChangeListener(this);
 
         if (DeviceUtils.isPhone(getActivity())) {
             mBarHeightLandscape_Text.setVisibility(View.GONE);
@@ -159,15 +188,26 @@ public class NavbarSettingsFragment extends Fragment implements SeekBar.OnSeekBa
     }
 
     @Override
-    public void onProgressChanged(SeekBar seekBar, int progress,
-                                    boolean fromUser) {
+    public void onProgressChanged(SeekBar seekbar, int progress, boolean fromUser) {
         if (fromUser) {
-            if (seekBar.getId() == R.id.navigation_bar_height) {
-                setNewHeight(seekBar, progress);
-            } else if (seekBar.getId() == R.id.navigation_bar_height_landscape) {
-                setNewHeight(seekBar, progress);
-            } else if (seekBar.getId() == R.id.navigation_bar_width) {
-                setNewHeight(seekBar, progress);
+            double min = 0;
+            double max = 0;
+
+            if (seekbar == mNavigationBarWidth) {
+                min = MIN_WIDTH_SCALAR * mDefaultWidth;
+                max = MAX_WIDTH_SCALAR * mDefaultWidth;
+                WValue = (int)(min + ((double)progress/100.0)*(max - min));
+                Settings.System.putInt(getActivity().getContentResolver(), Settings.System.NAVIGATION_BAR_WIDTH, WValue);
+            } else if (seekbar == mNavigationBarHeight) {
+                min = MIN_HEIGHT_SCALAR * mDefaultHeight;
+                max = MAX_HEIGHT_SCALAR * mDefaultHeight;
+                HValue = (int)(min + ((double)progress/100.0)*(max - min));
+                Settings.System.putInt(getActivity().getContentResolver(), Settings.System.NAVIGATION_BAR_HEIGHT, HValue);
+            } else if (seekbar == mNavigationBarHeightLandscape) {
+                min = MIN_HEIGHT_SCALAR * mDefaultHeightLandscape;
+                max = MAX_HEIGHT_SCALAR * mDefaultHeightLandscape;
+                LValue = (int)(min + ((double)progress/100.0)*(max - min));
+                Settings.System.putInt(getActivity().getContentResolver(), Settings.System.NAVIGATION_BAR_HEIGHT_LANDSCAPE, LValue);
             }
         }
     }
@@ -179,84 +219,5 @@ public class NavbarSettingsFragment extends Fragment implements SeekBar.OnSeekBa
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
         // time to write the values..
-    }
-
-    private void setNewHeight(SeekBar seekbar, int progress) {
-
-        if (seekbar == mNavigationBarWidth) {
-            int width = (percentToPixels(progress) + minimumBarSize);
-            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.NAVIGATION_BAR_WIDTH,
-                    width);
-            setInitValue(progress, seekbar);
-
-        } else if (seekbar == mNavigationBarHeight) {
-            int height = (percentToPixels(progress) + minimumBarSize);
-            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.NAVIGATION_BAR_HEIGHT,
-                    height);
-            setInitValue(progress, seekbar);
-
-        } else if (seekbar == mNavigationBarHeightLandscape) {
-            int height = (percentToPixels(progress) + minimumBarSize);
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.NAVIGATION_BAR_HEIGHT_LANDSCAPE,
-                    height);
-            setInitValue(progress, seekbar);
-        }
-    }
-
-    private void initHeightValues() {
-
-        // Navigation bar height
-        int customSize = Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.NAVIGATION_BAR_HEIGHT, defNavBarSize());
-        mNavigationBarHeight.setProgress((int)((float)customSize / (float)defNavBarSize() * 100.0f));
-        mNavigationBarHeight.setOnSeekBarChangeListener(this);
-
-        // Navigation bar height - landscape
-        customSize = Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.NAVIGATION_BAR_HEIGHT_LANDSCAPE, defNavBarSize());
-        mNavigationBarHeightLandscape.setProgress((int)((float)customSize / (float)defNavBarSize() * 100.0f));
-        mNavigationBarHeightLandscape.setOnSeekBarChangeListener(this);
-
-        // Navigation bar width (phones only)
-        customSize = Settings.System.getInt(getActivity()
-                .getContentResolver(), Settings.System.NAVIGATION_BAR_WIDTH, defNavBarSize());
-        mNavigationBarWidth.setProgress((int)((float)customSize / (float)defNavBarSize() * 100.0f));
-        mNavigationBarWidth.setOnSeekBarChangeListener(this);
-    }
-
-    private int defNavBarSize() {
-
-        if (firstShot) {
-            DisplayMetrics metrics = new DisplayMetrics();
-            float mScreenSize = (getResources().getDisplayMetrics().density
-                        * metrics.DENSITY_DEFAULT);
-
-            preSizer = getResources().getDimensionPixelSize(R.dimen.navigation_bar_height_default);
-            defNavBarSize = R.dimen.navigation_bar_height_default + (int)(preSizer * 1.5);
-            minimumBarSize = (int)(mScreenSize * 0.08);
-
-            if (mScreenSize >= metrics.DENSITY_XXHIGH) {
-                defNavBarSize = preSizer + 60;
-            } else if (mScreenSize < metrics.DENSITY_XXHIGH && mScreenSize >= metrics.DENSITY_XHIGH) {
-                defNavBarSize = preSizer + 20;
-            }
-            firstShot = false;
-        }
-        return defNavBarSize;
-    }
-
-    private void setInitValue(int progress, SeekBar seekBar) {
-        if (seekBar.getId() == R.id.navigation_bar_height) {
-            HValue = progress;
-        } else if (seekBar.getId() == R.id.navigation_bar_height_landscape) {
-            LValue = progress;
-        } else if (seekBar.getId() == R.id.navigation_bar_width) {
-            WValue = progress;
-        }
-    }
-
-    private int percentToPixels(int percent) {
-        return (int)((float)defNavBarSize() * ((float) percent * 0.01f)); 
     }
 }
