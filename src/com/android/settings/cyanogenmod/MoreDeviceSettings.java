9b49b7f4e61f5e7752329479c9a69beb6bf49657
PrimeDirective <activethrasher00@gmail.com>
2014-05-15 15:03:46 -0400

    Refactor Device/More... to voodoo/Hardware:
    this accomplishes 3 goals:
    move all hardware related features to a common header
    comply with google development guidelines for number of entries per category
    place non-aosp "voodoo" under our ROM settings category for better organization
    
    to do: create system header to match layout style for non interface features
    
    Change-Id: I7821dae6d8d3ba3e23d11cf7a89b26e3a35879f2

--- src/com/android/settings/cyanogenmod/MoreDeviceSettings.java
+++ src/com/android/settings/cyanogenmod/MoreDeviceSettings.java
@@ -18,20 +18,34 @@ package com.android.settings.cyanogenmod;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.SystemProperties;
+import android.os.Vibrator;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-import com.android.settings.vanir.HeaderCompatCheck;
+import com.android.settings.hardware.DisplayColor;
+import com.android.settings.hardware.DisplayGamma;
+import com.android.settings.hardware.VibratorIntensity;
+import com.vanir.util.CMDProcessor;
 
+import org.cyanogenmod.hardware.AdaptiveBacklight;
+import org.cyanogenmod.hardware.ColorEnhancement;
 import org.cyanogenmod.hardware.DisplayGammaCalibration;
+import org.cyanogenmod.hardware.TapToWake;
+
+import java.io.File;
 
 public class MoreDeviceSettings extends SettingsPreferenceFragment {
     private static final String TAG = "MoreDeviceSettings";
@@ -40,11 +54,25 @@ public class MoreDeviceSettings extends SettingsPreferenceFragment {
     private static final String KEY_DISPLAY_CALIBRATION_CATEGORY = "display_calibration_category";
     private static final String KEY_DISPLAY_COLOR = "color_calibration";
     private static final String KEY_DISPLAY_GAMMA = "gamma_tuning";
-    private static final String FORCE_HIGHEND_GFX_PREF = "pref_force_highend_gfx";
-    private static final String FORCE_HIGHEND_GFX_PERSIST_PROP = "persist.sys.force_highendgfx";
+    private static final String KEY_ADAPTIVE_BACKLIGHT = "adaptive_backlight";
+    private static final String KEY_COLOR_ENHANCEMENT = "color_enhancement";
+    private static final String KEY_SCREEN_COLOR_SETTINGS = "screencolor_settings";
     private static final String KEY_SCREEN_GESTURE_SETTINGS = "touch_screen_gesture_settings";
+    private static final String KEY_TAP_TO_WAKE = "double_tap_wake_gesture";
+    private static final String KEY_BUTTON_NAVIGATION = "buttons_navigation";
+    public static final String PREF_FCHARGE = "fast_charge";
+    private static final String FCHARGE_PATH = "sys/kernel/fast_charge/force_fast_charge";
+    private static final String KEY_ADVANCED_DISPLAY_SETTINGS = "advanced_display_settings";
+    private static final String KEY_PROXIMITY_WAKE = "proximity_on_wake";
 
-    private CheckBoxPreference mForceHighEndGfx;
+    private CheckBoxPreference mTapToWake;
+    private static CheckBoxPreference mFastCharge;
+    private CheckBoxPreference mAdaptiveBacklight;
+    private CheckBoxPreference mColorEnhancement;
+
+    private Preference mAdvanced;
+    private Preference mNavigation;
+    private PreferenceScreen mScreenColorSettings;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,49 +84,209 @@ public class MoreDeviceSettings extends SettingsPreferenceFragment {
         final PreferenceGroup calibrationCategory =
                 (PreferenceGroup) findPreference(KEY_DISPLAY_CALIBRATION_CATEGORY);
 
-        if (!HeaderCompatCheck.hasVibratorIntensity(getActivity())) {
+        final int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        mNavigation = (Preference) findPreference(KEY_BUTTON_NAVIGATION);
+        if (deviceKeys == 0) {
+            getPreferenceScreen().removePreference(mNavigation);
+        }
+
+        // this fast charge commit is required for this feature
+        // https://github.com/jimsth/vanir_hammerhead/commit/44c571eda5afc309d6862ab76a9400518ade3be2
+        mFastCharge = (CheckBoxPreference) findPreference(PREF_FCHARGE);
+        if (!exists(FCHARGE_PATH)) {
+            getPreferenceScreen().removePreference(mFastCharge);
+        }
+
+        Vibrator vibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        boolean hasvib = VibratorIntensity.isSupported() && vibrator != null && vibrator.hasVibrator();
+        if (!hasvib) {
             removePreference(KEY_SENSORS_MOTORS_CATEGORY);
         }
 
-        boolean colors = HeaderCompatCheck.modifiableDisplayColors(getActivity());
-        boolean gamma = HeaderCompatCheck.modifiableDisplayGamma(getActivity());
+        // this was abandoned by CM.. but i guess we'll keep it anyways even though kernel apps do it too
+        mTapToWake = (CheckBoxPreference) findPreference(KEY_TAP_TO_WAKE);
+        if (!isTapToWakeSupported()) {
+            getPreferenceScreen().removePreference(mTapToWake);
+            mTapToWake = null;
+        }
+        if (isTapToWakeSupported()) {
+            getPreferenceScreen().removePreference(findPreference(KEY_PROXIMITY_WAKE));
+            Settings.System.putInt(resolver, Settings.System.PROXIMITY_ON_WAKE, 1);
+        }
+
+        mAdaptiveBacklight = (CheckBoxPreference) findPreference(KEY_ADAPTIVE_BACKLIGHT);
+        mColorEnhancement = (CheckBoxPreference) findPreference(KEY_COLOR_ENHANCEMENT);
+        mScreenColorSettings = (PreferenceScreen) findPreference(KEY_SCREEN_COLOR_SETTINGS);
+        mAdvanced = getPreferenceScreen().findPreference(KEY_ADVANCED_DISPLAY_SETTINGS);
+
+        boolean colors = DisplayColor.isSupported();
+        boolean gamma = DisplayGamma.isSupported();
         boolean WTF = DisplayGammaCalibration.getNumberOfControls() == 0;
 
-        if (!gamma && !colors) {
-            getPreferenceScreen().removePreference(calibrationCategory);
-        } else if (!gamma) {
+        if (!gamma || WTF) {
             calibrationCategory.removePreference(findPreference(KEY_DISPLAY_GAMMA));
-        } else if (!colors) {
+        }
+        if (!colors) {
             calibrationCategory.removePreference(findPreference(KEY_DISPLAY_COLOR));
         }
-
-        if (WTF) {
-            // yay lets submit half-finished code and rely on the community to do the rest of the work
-            calibrationCategory.removePreference(findPreference(KEY_DISPLAY_GAMMA));
+        if (!isAdaptiveBacklightSupported()) {
+            calibrationCategory.removePreference(mAdaptiveBacklight);
+            mAdaptiveBacklight = null;
+        }
+        if (!isColorEnhancementSupported()) {
+            calibrationCategory.removePreference(mColorEnhancement);
+            mColorEnhancement = null;
+        }
+        if (!isPostProcessingSupported()) {
+            calibrationCategory.removePreference(mScreenColorSettings);
+            mScreenColorSettings = null;
         }
 
-        if (HeaderCompatCheck.isLowRam(getActivity())) {
-            mForceHighEndGfx = (CheckBoxPreference) findPreference(FORCE_HIGHEND_GFX_PREF);
-            String forceHighendGfx = SystemProperties.get(FORCE_HIGHEND_GFX_PERSIST_PROP, "false");
-            mForceHighEndGfx.setChecked("true".equals(forceHighendGfx));
-        } else {
-            getPreferenceScreen().removePreference(findPreference(FORCE_HIGHEND_GFX_PREF));
-        }
-        
         Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
-                getPreferenceScreen(), KEY_SCREEN_GESTURE_SETTINGS);
+                calibrationCategory, KEY_ADVANCED_DISPLAY_SETTINGS);
+
+        Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
+        getPreferenceScreen(), KEY_SCREEN_GESTURE_SETTINGS);
+
+        boolean emptyList = mColorEnhancement == null
+                && mScreenColorSettings == null
+                && mAdaptiveBacklight == null
+                && mAdvanced == null;
+
+        if ((!gamma || WTF) && !colors
+                && !isAdaptiveBacklightSupported()
+                && !isColorEnhancementSupported()
+                && !isPostProcessingSupported()) {
+            getPreferenceScreen().removePreference(calibrationCategory);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mTapToWake != null) {
+            mTapToWake.setChecked(TapToWake.isEnabled());
+        }
+        if (mAdaptiveBacklight != null) {
+            mAdaptiveBacklight.setChecked(AdaptiveBacklight.isEnabled());
+        }
+        if (mColorEnhancement != null) {
+            mColorEnhancement.setChecked(ColorEnhancement.isEnabled());
+        }
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mForceHighEndGfx) {
-            SystemProperties.set(FORCE_HIGHEND_GFX_PERSIST_PROP,
-                    mForceHighEndGfx.isChecked() ? "true" : "false");
+        if (preference == mAdaptiveBacklight) {
+            return AdaptiveBacklight.setEnabled(mAdaptiveBacklight.isChecked());
+
+        } else if (preference == mColorEnhancement) {
+            return ColorEnhancement.setEnabled(mColorEnhancement.isChecked());
+
+        } else if (preference == mTapToWake) {
+            return TapToWake.setEnabled(mTapToWake.isChecked());
+
+        } else if (preference == mFastCharge) {
+            writeFastChargeOption();
+
         } else {
             // If we didn't handle it, let preferences handle it.
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
+        return false;
+    }
 
-        return true;
+    private static boolean isTapToWakeSupported() {
+        try {
+            return TapToWake.isSupported();
+        } catch (NoClassDefFoundError e) {
+            // Hardware abstraction framework not installed
+            return false;
+        }
+    }
+
+    private void writeFastChargeOption() {
+
+        if (mFastCharge.isChecked()) {
+            new CMDProcessor().su.runWaitFor("busybox echo 0 > /sys/kernel/fast_charge/force_cast_charge");
+        } else {
+            new CMDProcessor().su.runWaitFor("busybox echo 1 > /sys/kernel/fast_charge/force_cast_charge");
+        }
+    }
+
+    private static boolean exists(String string) {
+        File f = new File(string);
+        if (f.exists()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Restore the properties associated with this preference on boot
+     * @param ctx A valid context
+     */
+    public static void restore(Context ctx) {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+
+        if (isTapToWakeSupported()) {
+            final boolean enabled = prefs.getBoolean(KEY_TAP_TO_WAKE, true);
+            if (!TapToWake.setEnabled(enabled)) {
+                Log.e(TAG, "Failed to restore tap-to-wake settings.");
+            } else {
+                Log.d(TAG, "Tap-to-wake settings restored.");
+            }
+        }
+
+        if (isColorEnhancementSupported()) {
+            final boolean enabled = prefs.getBoolean(KEY_COLOR_ENHANCEMENT, true);
+            if (!ColorEnhancement.setEnabled(enabled)) {
+                Log.e(TAG, "Failed to restore color enhancement settings.");
+            } else {
+                Log.d(TAG, "Color enhancement settings restored.");
+            }
+        }
+
+        if (isAdaptiveBacklightSupported()) {
+            final boolean enabled = prefs.getBoolean(KEY_ADAPTIVE_BACKLIGHT, true);
+            if (!AdaptiveBacklight.setEnabled(enabled)) {
+                Log.e(TAG, "Failed to restore adaptive backlight settings.");
+            } else {
+                Log.d(TAG, "Adaptive backlight settings restored.");
+            }
+        }
+    }
+    
+    private boolean isPostProcessingSupported() {
+        boolean ret = true;
+        final PackageManager pm = getPackageManager();
+        try {
+            pm.getPackageInfo("com.qualcomm.display", PackageManager.GET_META_DATA);
+        } catch (NameNotFoundException e) {
+            ret = false;
+        }
+        return ret;
+    }
+
+    private static boolean isAdaptiveBacklightSupported() {
+        try {
+            return AdaptiveBacklight.isSupported();
+        } catch (NoClassDefFoundError e) {
+            // Hardware abstraction framework not installed
+            return false;
+        }
+    }
+
+    private static boolean isColorEnhancementSupported() {
+        try {
+            return ColorEnhancement.isSupported();
+        } catch (NoClassDefFoundError e) {
+            // Hardware abstraction framework not installed
+            return false;
+        }
     }
 }
