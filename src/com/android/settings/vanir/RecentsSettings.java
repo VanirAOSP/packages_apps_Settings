45aaa1b3baddc98ca78c55d33cc314f098b469a7
Dave Kessler <activethrasher00@gmail.com>
2015-01-03 10:09:03 -0800

    Recents improvements [2/2]
    PS2: cleanup, improve, get rid of redundancy -> AlmightyMegadeth00
    
    Change-Id: Ibd7d6c33f90af8963d67643b5671bcee97a896b4
    Signed-off-by: DHOMD <DHO@dho.im>
    Conflicts:
    	res/values/vanir_arrays.xml
    	res/values/vanir_strings.xml
    	res/xml/vanir_interface.xml

---
+++ src/com/android/settings/vanir/RecentsSettings.java
@@ -0,0 +1,68 @@
+/*
+* Copyright (C) 2014 The CyanogenMod Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.settings.vanir;
+
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class RecentsSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+    private ListPreference mRecentsClearAllLocation;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.recents_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mRecentsClearAllLocation = (ListPreference) prefSet.findPreference(RECENTS_CLEAR_ALL_LOCATION);
+        int location = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 1, UserHandle.USER_CURRENT);
+        mRecentsClearAllLocation.setValue(String.valueOf(location));
+        setListPreferenceSummary(mRecentsClearAllLocation, String.valueOf(location));
+        mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mRecentsClearAllLocation) {
+            int location = Integer.valueOf((String) objValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
+            setListPreferenceSummary(mRecentsClearAllLocation, (String) objValue);
+            return true;
+        }
+        return false;
+    }
+
+    private void setListPreferenceSummary(final ListPreference pref, final String value) {
+        pref.setSummary(pref.getEntries()[pref.findIndexOfValue(value)]);
+    }
+}
