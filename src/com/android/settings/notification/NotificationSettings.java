799e9f470e55d3d9468af59f41f2cb07559d0bc3
nuclearmistake <nuclearmistake@gmail.com>
2015-01-31 10:29:23 -0500

    Revert "Settings: forward port HeadsUp options (2/2)"
    This reverts commit 0b43df3e04a6063d8fd97212428212d2ab432627.
    
    Conflicts:
    
    	AndroidManifest.xml
    
    Change-Id: Iafe8b8690e04baecddfc8aeda4911ccd22357072

--- src/com/android/settings/notification/NotificationSettings.java
+++ src/com/android/settings/notification/NotificationSettings.java
@@ -106,7 +106,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
     private Preference mNotificationAccess;
     private boolean mSecure;
     private int mLockscreenSelectedValue;
-    private Preference mHeadsUp;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -146,8 +145,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
 
         mNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);
         refreshNotificationListeners();
-
-        mHeadsUp = findPreference(Settings.System.HEADS_UP_NOTIFICATION);
     }
 
     @Override
@@ -157,11 +154,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
         lookupRingtoneNames();
         updateNotificationPreferenceState();
         mSettingsObserver.register(true);
-
-        boolean headsUpEnabled = Settings.System.getInt(
-                getContentResolver(), Settings.System.HEADS_UP_NOTIFICATION, 0) == 1;
-        mHeadsUp.setSummary(headsUpEnabled
-                ? R.string.summary_heads_up_enabled : R.string.summary_heads_up_disabled);
     }
 
     @Override
