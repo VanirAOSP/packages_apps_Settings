87082fa18cbfb837f67e4943de8aeb4a24a3e67a
ejjohnson1999 <ej.johnson1999@gmail.com>
2016-07-13 22:40:04 -0700

    add meminfo and cpuinfo to about phone
    I could not find a commit of just cpuinfo and meminfo so I pieced this together
    my self as the other parts that people add to about phone with this in there
    is useless to us
    
    Change-Id: I33f7e76eca253b82d930a140bbfabfeb8043c037

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -66,6 +66,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String LOG_TAG = "DeviceInfoSettings";
     private static final String FILENAME_PROC_VERSION = "/proc/version";
     private static final String FILENAME_MSV = "/sys/board_properties/soc/msv";
+    private static final String FILENAME_PROC_MEMINFO = "/proc/meminfo";
+    private static final String FILENAME_PROC_CPUINFO = "/proc/cpuinfo";
 
     private static final String KEY_MANUAL = "manual";
     private static final String KEY_REGULATORY_INFO = "regulatory_info";
@@ -73,6 +75,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_URL_SAFETYLEGAL = "ro.url.safetylegal";
     private static final String PROPERTY_SELINUX_STATUS = "ro.build.selinux";
     private static final String KEY_KERNEL_VERSION = "kernel_version";
+    private static final String KEY_DEVICE_CPU = "device_cpu";
+    private static final String KEY_DEVICE_MEMORY = "device_memory";
     private static final String KEY_BUILD_NUMBER = "build_number";
     private static final String KEY_DEVICE_MODEL = "device_model";
     private static final String KEY_DEVICE_NAME = "device_name";
@@ -145,6 +149,21 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         } else if (!SELinux.isSELinuxEnforced()) {
             String status = getResources().getString(R.string.selinux_status_permissive);
             setStringSummary(KEY_SELINUX_STATUS, status);
+        }
+
+        final String cpuInfo = getCPUInfo();
+        String memInfo = getMemInfo();
+
+        if (cpuInfo != null) {
+            setStringSummary(KEY_DEVICE_CPU, cpuInfo);
+        } else {
+            getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_CPU));
+        }
+
+        if (memInfo != null) {
+            setStringSummary(KEY_DEVICE_MEMORY, memInfo);
+        } else {
+            getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_MEMORY));
         }
 
         setStringSummary(KEY_DEVICE_NAME, Build.PRODUCT);
@@ -561,6 +580,72 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 return SystemProperties.get(property).equals("");
             }
         };
+
+    private String getMemInfo() {
+        String result = null;
+        BufferedReader reader = null;
+
+        try {
+            /* /proc/meminfo entries follow this format:
+             * MemTotal:         362096 kB
+             * MemFree:           29144 kB
+             * Buffers:            5236 kB
+             * Cached:            81652 kB
+             */
+            String firstLine = readLine(FILENAME_PROC_MEMINFO);
+            if (firstLine != null) {
+                String parts[] = firstLine.split("\\s+");
+                if (parts.length == 3) {
+                    result = Long.parseLong(parts[1])/1024 + " MB";
+                }
+            }
+        } catch (IOException e) {}
+
+        return result;
+    }
+ }
+
+    private String getCPUInfo() {
+        String result = null;
+        int coreCount = 0;
+
+        try {
+            /* The expected /proc/cpuinfo output is as follows:
+             * Processor	: ARMv7 Processor rev 2 (v7l)
+             * BogoMIPS	: 272.62
+             * BogoMIPS	: 272.62
+             *
+             * On kernel 3.10 this changed, it is now the last
+             * line. So let's read the whole thing, search
+             * specifically for "Processor" or "model name"
+             * and retain the old
+             * "first line" as fallback.
+             * Also, use "processor : <id>" to count cores
+             */
+            BufferedReader ci = new BufferedReader(new FileReader(FILENAME_PROC_CPUINFO));
+            String firstLine = ci.readLine();
+            String latestLine = firstLine;
+            while (latestLine != null) {
+                if (latestLine.startsWith("Processor")
+                    || latestLine.startsWith("model name"))
+                  result = latestLine.split(":")[1].trim();
+                if (latestLine.startsWith("processor"))
+                  coreCount++;
+                latestLine = ci.readLine();
+            }
+            if (result == null && firstLine != null) {
+                result = firstLine.split(":")[1].trim();
+            }
+            /* Don't do this. hotplug throws off the count
+            if (coreCount > 1) {
+                result = result + " (x" + coreCount + ")";
+            }
+            */
+            ci.close();
+        } catch (IOException e) {}
+
+        return result;
+    }
 
     private boolean removePreferenceIfPackageNotInstalled(Preference preference) {
         String intentUri=((PreferenceScreen) preference).getIntent().toUri(1);
