92d5cc9c0121b8aa87ccd8f5ae195e1c9aad75cc
DHOMD <DHO@dho.im>
2016-12-08 20:10:28 -0500

    Settings App Catagory
    Change-Id: Ia9558e5b73efd044cd410fe4a8e32508390e1e54

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -161,6 +161,7 @@ public class Settings extends SettingsActivity {
     // Categories.
     public static class WirelessSettings extends SettingsActivity { /* empty */ }
     public static class VanirSettings extends SettingsActivity { /* empty */ }
+    public static class AppsSettings extends SettingsActivity { /* empty */ }
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
